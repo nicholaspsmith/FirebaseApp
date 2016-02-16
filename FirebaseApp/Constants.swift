ac7563eee54c13c6d684f97bfd04a47385ba2b38
Nicholas Smith <npsmith1990@gmail.com>
2016-02-15 23:22:35 -0600

    Create material design style

---
+++ FirebaseApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  FirebaseApp
+//
+//  Created by Nick on 2/15/16.
+//  Copyright © 2016 Nicholas Smith. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
