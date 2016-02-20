66f42b1d46cf4158b4c51cbf0fdd29ad1582aa45
Nicholas Smith <npsmith1990@gmail.com>
2016-02-19 18:28:04 -0600

    Add firebase ref urls

--- FirebaseApp/DataService.swift
+++ FirebaseApp/DataService.swift
@@ -9,10 +9,14 @@
 import Foundation
 import Firebase
 
+let URL_BASE = "https://ios-firebaseapp.firebaseio.com"
+
 class DataService {
     static let ds = DataService()
     
-    private var _REF_BASE = Firebase(url: "https://ios-firebaseapp.firebaseio.com")
+    private var _REF_BASE = Firebase(url: "\(URL_BASE)")
+    private var _REF_POSTS = Firebase(url: "\(URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(URL_BASE)/users")
     
     var REF_BASE: Firebase {
         return _REF_BASE
