478e054a4adf961b1293dcdaa546704df6873cda
Nicholas Smith <npsmith1990@gmail.com>
2016-02-20 09:43:35 -0600

    Set contents of cell likes and description upon load from firebase

--- FirebaseApp/PostCell.swift
+++ FirebaseApp/PostCell.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class PostCell: UITableViewCell {
     
 
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var showcaseImg: UIImageView!
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,10 +30,11 @@ class PostCell: UITableViewCell {
         showcaseImg.clipsToBounds = true
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureCell(post: Post) {
+        self.post = post
+        
+        self.descriptionText.text = post.postDescription
+        self.likesLbl.text = "\(post.likes)"
     }
 
 }
