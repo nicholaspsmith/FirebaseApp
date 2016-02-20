aedf1096bee8234cca70c5aea4899717ce59e3ed
Nicholas Smith <npsmith1990@gmail.com>
2016-02-19 20:34:52 -0600

    Create new class for feed table view

---
+++ FirebaseApp/FeedVC.swift
@@ -0,0 +1,35 @@
+//
+//  FeedVC.swift
+//  FirebaseApp
+//
+//  Created by Nick on 2/19/16.
+//  Copyright © 2016 Nicholas Smith. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+
+}
