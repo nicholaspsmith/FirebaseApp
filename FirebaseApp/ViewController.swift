f46a30cab3789e29ad50da644d56e3e0ec0b0b7a
Nicholas Smith <npsmith1990@gmail.com>
2016-02-19 20:50:00 -0600

    Create user in firebase for email user

--- FirebaseApp/ViewController.swift
+++ FirebaseApp/ViewController.swift
@@ -74,7 +74,15 @@ class ViewController: UIViewController {
                             } else {
                                 NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
                                 
-                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+                                
+                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: {
+                                    err, authData in
+                                    
+                                    let user = ["provider": authData.provider!]
+                                    DataService.ds.createFirebaseUser(authData.uid, user: user)
+                                    
+                                })
+                                
                                 self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                             }
                             
