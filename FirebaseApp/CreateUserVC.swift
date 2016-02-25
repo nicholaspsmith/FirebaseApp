5e238ade4cd08704693cb9c67a442f851a8e71c2
Nick P Smith <npsmith1990@gmail.com>
2016-02-25 15:10:50 -0600

    Create new user via email working

--- FirebaseApp/CreateUserVC.swift
+++ FirebaseApp/CreateUserVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class CreateUserVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -38,30 +39,72 @@ class CreateUserVC: UIViewController, UIImagePickerControllerDelegate, UINavigat
     }
 
     @IBAction func createUser(sender: AnyObject) {
-        let username = usernameTextField.text
-        let password = passwordTextField.text
+        let username = usernameTextField.text!
+        let imgUrl = profileImg
         
-        DataService.ds.REF_BASE.createUser(username, password: password, withValueCompletionBlock: { error, result in
-            
-            if error != nil {
-                // There was a problem creating this user account
-                print(error.debugDescription)
-                self.showErrorAlert("Could not create account", msg: "Try a different username or password")
-            } else {
-                NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
-                
-                DataService.ds.REF_BASE.authUser(username, password: password, withCompletionBlock: { err, authData in
-                    let user = [
-                        "username": username!,
-                        "provider": authData.provider!
-                    ]
-                    DataService.ds.createFirebaseUser(authData.uid, user: user)
-                })
-                
-                self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
-            }
+        // @TODO: NEED TO PULL pw and username from previous view
+        let password = passwordTextField.text!
+        let email = "\(username)@gmail.com"
+        
+        print(imgUrl)
+        
+        DataService.ds.REF_USERS.createUser(email, password: password,
+            withValueCompletionBlock: { error, result in
+                if error != nil {
+                    // There was an error creating the account
+                    print(error.debugDescription)
+                } else {
+                    let uid = result["uid"] as? String
+                    print("Successfully created user account with uid: \(uid)")
+                    
+                    DataService.ds.REF_USERS.childByAppendingPath(uid).childByAppendingPath("username").setValue(username)
+                    DataService.ds.REF_USERS.childByAppendingPath(uid).childByAppendingPath("provider").setValue("password")
+                    
+                    self.uploadImage(uid!)
+                    
+                    NSUserDefaults.standardUserDefaults().setValue(uid, forKey: KEY_UID)
+
+                    self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                }
         })
         
+    }
+    
+    func uploadImage(uid: String) {
+        if let img = profileImg.image where imageSet == true {
+            
+            let urlStr = "https://post.imageshack.us/upload_api.php"
+            let url = NSURL(string: urlStr)!
+            let imgData = UIImageJPEGRepresentation(img, 0.2)!
+            let keyData = "12DJKPSU5fc3afbd01b1630cc718cae3043220f3".dataUsingEncoding(NSUTF8StringEncoding)!
+            let keyJSON = "json".dataUsingEncoding(NSUTF8StringEncoding)!
+            
+            Alamofire.upload(.POST, url, multipartFormData: { multipartFormData in
+                multipartFormData.appendBodyPart(data: imgData, name: "fileupload", fileName: "image", mimeType: "image/jpg")
+                multipartFormData.appendBodyPart(data: keyData, name: "key")
+                multipartFormData.appendBodyPart(data: keyJSON, name: "format")
+                
+                }) { encodingResult in
+                    
+                    switch encodingResult {
+                    case .Success(let upload, _, _):
+                        upload.responseJSON(completionHandler: { (response) in
+                            if let info = response.result.value as? Dictionary<String, AnyObject> {
+                                if let links = info["links"] as? Dictionary<String, AnyObject> {
+                                    if let imgLink = links["image_link"] as? String {
+                                        DataService.ds.REF_USERS.childByAppendingPath(uid).childByAppendingPath("imageUrl").setValue(imgLink)
+                                    }
+                                }
+                            }
+                        })
+                        
+                    case .Failure(let error):
+                        print(error)
+                    }
+            }//Alamofire.upload
+            
+        }
+
     }
 
 
